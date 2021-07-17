2d2884b83d34b2964d9ccd12896ba9882ca26bcf
violapterin <violapterin@gmail.com>
2021-07-18 04:54:27 +0800

    Longest nonrepeating substring: Pass compilation

--- cpp-medium/longest-nonrepeating-substring.cpp
+++ cpp-medium/longest-nonrepeating-substring.cpp
@@ -8,56 +8,57 @@ without repeating characters.
 
 #include <iostream>
 #include <string>
-#include <set>
-typedef std::set<char> Many_symbol;
-typedef std::tuple<int, int, Many_symbol> Record;
+#include <algorithm>
+#include <tuple>
+typedef std::tuple<int, int, std::string> Record;
 int find_length_longest_substring(std::string);
 Record update_record(char, Record);
 
 int main()
 {
-   text = "abbcdb";
-   size = find_length_longest_substring(text);
-   std::cout << size << std::endl;
+   std::string text = "abbcdb";
+   int length = find_length_longest_substring(text);
+   std::cout << length << std::endl;
    // "3"
    /* substring: "bcd" */
 }
 
 int find_length_longest_substring(std::string text)
 {
-   Record record = std::make_tuple(0, 0, Many_symbol());
-   for (int count = 0; count <= text.size(); count++)
+   Record record = std::make_tuple(0, 0, std::string());
+   for (int count = 0; count < text.size(); count++)
    {
       char symbol = text[count];
       update_record(symbol, record);
    }
-   length_detached_longest = std::get<0>(record);
+   int length_detached_longest = std::get<0>(record);
    return length_detached_longest;
 }
 
 Record update_record(char symbol_new, Record record_old)
 {
-   int length_detached = std::get<0>(record_old);
-   int length_connected = std::get<1>(record_old);
-   Many_symbol many_symbol = std::get<2>(record_old);
-   if (symbol_new)
-   if (many_symbol.find(symbol_new) == many_symbol.end())
+   int size_detached = std::get<0>(record_old);
+   int size_connected = std::get<1>(record_old);
+   std::string series = std::get<2>(record_old);
+   int cut = series.find_last_of(symbol_new);
+   std::string previous;
+   if (cut != std::string::npos)
    {
-      many_symbol.insert(symbol_new);
-      length_connected += 1;
+      previous = series.substr(cut, std::string::npos);
    }
    else
    {
-      length_detached += 1;
+      previous = series;
    }
-   if (length_detached < length_connected)
-   {
-      length_detached = length_connected;
-   }
+   series = previous + symbol_new;
+   std::cout << "series:" << series << std::endl;
+   size_connected == series.length();
+   size_detached = std::max(size_detached, size_connected);
+   std::cout << "detached:" << size_detached << std::endl;
    Record record_new = std::make_tuple(
-      length_detached,
-      length_connected,
-      many_symbol
+      size_detached,
+      size_connected,
+      series 
    );
    return record_new;
 }
