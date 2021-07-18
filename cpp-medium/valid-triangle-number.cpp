8edf8a3efdba441b5b98b2549218634bc703baaa
violapterin <violapterin@gmail.com>
2021-07-18 22:47:48 +0800

    Valid triangle number: Correct, but exceed time

--- cpp-medium/valid-triangle-number.cpp
+++ cpp-medium/valid-triangle-number.cpp
@@ -9,14 +9,64 @@ as side lengths of a triangle.
 
 #include <iostream>
 #include <vector>
-typedef std::vector<int> Array;
-int count_triangle_number(Array&);
+#include <algorithm>
+typedef std::vector<int> Choice;
+int count_triangle_number(Choice&);
+int count_triangle_number_variable(Choice);
+int count_triangle_number_fixed(int, Choice);
 
 int main()
 {
-   int array = {2, 2, 3, 4};
-   int count = count_triangle_number(Array&);
+   Choice choice = {2, 2, 3, 4};
+   int count = count_triangle_number(choice);
    std::cout << count << std::endl;
    // "3"
    /* (2,3,4), (2,3,4), (2,2,3) */
+}
+
+int count_triangle_number(Choice& choice)
+{
+   std::sort(choice.begin(), choice.end());
+   int count = count_triangle_number_variable(choice);
+   return count;
+}
+
+int count_triangle_number_variable(Choice choice)
+{
+   if (choice.size() < 3)
+   {
+      return 0;
+   }
+   int small = choice[0];
+   choice.erase(choice.begin());
+   int count_partial = count_triangle_number_variable(choice);
+   int count_fixed = count_triangle_number_fixed(small, choice);
+   int count = count_fixed + count_partial;
+   return count;
+}
+
+int count_triangle_number_fixed(int small, Choice choice)
+{
+   if (choice.size() < 2)
+   {
+      return 0;
+   }
+   int count = 0;
+   for (
+      auto less_ = choice.begin();
+      less_ != choice.end() - 1; less_++
+   )
+   {
+      for (
+         auto greater_ = less_ + 1;
+         greater_ != choice.end(); greater_++
+      )
+      {
+         if (*greater_ < *less_ + small)
+         {
+            count += 1;
+         }
+      }
+   }
+   return count;
 }
