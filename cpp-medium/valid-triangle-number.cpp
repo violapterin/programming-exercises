07b18b910a63910f4dda85763a411e72de3cbbe7
violapterin <violapterin@gmail.com>
2021-07-19 00:45:02 +0800

    Valid triangle: Get accepted

--- cpp-medium/valid-triangle-number.cpp
+++ cpp-medium/valid-triangle-number.cpp
@@ -6,14 +6,15 @@ as side lengths of a triangle.
    `1 <= choice.length <= 1000`
    `0 <= choice[i] <= 1000`
 */
+// Accepted July 19, 2021.
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 typedef std::vector<int> Choice;
 int count_triangle_number(Choice&);
-int count_triangle_number_variable(Choice);
-int count_triangle_number_fixed(int, Choice);
+int count_triangle_number_variable(Choice*);
+int count_triangle_number_fixed(int, Choice*);
 
 int main()
 {
@@ -27,45 +28,43 @@ int main()
 int count_triangle_number(Choice& choice)
 {
    std::sort(choice.begin(), choice.end());
-   int count = count_triangle_number_variable(choice);
+   int count = count_triangle_number_variable(&choice);
    return count;
 }
 
-int count_triangle_number_variable(Choice choice)
+int count_triangle_number_variable(Choice* choice_)
 {
-   if (choice.size() < 3)
+   if (choice_->size() < 3)
    {
       return 0;
    }
-   int small = choice[0];
-   choice.erase(choice.begin());
-   int count_partial = count_triangle_number_variable(choice);
-   int count_fixed = count_triangle_number_fixed(small, choice);
+   int small = (*choice_)[0];
+   choice_->erase(choice_->begin());
+   int count_fixed = count_triangle_number_fixed(small, choice_);
+   int count_partial = count_triangle_number_variable(choice_);
    int count = count_fixed + count_partial;
    return count;
 }
 
-int count_triangle_number_fixed(int small, Choice choice)
+int count_triangle_number_fixed(int small, Choice* choice_)
 {
-   if (choice.size() < 2)
+   if (choice_->size() < 2)
    {
       return 0;
    }
    int count = 0;
    for (
-      auto less_ = choice.begin();
-      less_ != choice.end() - 1; less_++
+      auto less_ = choice_->begin();
+      less_ != choice_->end() - 1; less_++
    )
    {
       for (
          auto greater_ = less_ + 1;
-         greater_ != choice.end(); greater_++
+         greater_ != choice_->end(); greater_++
       )
       {
-         if (*greater_ < *less_ + small)
-         {
-            count += 1;
-         }
+         if (*greater_ >= *less_ + small) { break; }
+         count += 1;
       }
    }
    return count;
