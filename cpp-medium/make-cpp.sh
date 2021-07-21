1762191547c7c49ce0b10f24f2df8d682abf443e
violapterin <violapterin@gmail.com>
2021-07-22 05:08:58 +0800

    Interleaving string: Import description

--- cpp-medium/make-cpp.sh
+++ cpp-medium/make-cpp.sh
@@ -7,46 +7,54 @@ update() {
    suffix_in=.cpp
    suffix_out=.o
    for path_in in $1/*; do
-      if [ ! -f $path_in ]; then
+      if [ ! -f $path_in ]
+      then
          continue
       fi
-      if  [[ $path_in != *${suffix_in} ]] ; then
+      if  [ $path_in != *$suffix_in ]
+      then
          continue
       fi
-      name="${path_in##*/}"
-      bare="${name%$suffix_in}"
-      path_out="$2/$bare$suffix_out"
-      if [ -f $path_out ]; then
+      name=${path_in##*/}
+      bare=${name%$suffix_in}
+      path_out=$2/${bare}${suffix_out}
+      if [ -f $path_out ]
+      then
          # # Compare time stamps.
-         if [ $path_out -nt $path_in ]; then
+         if [ $path_out -nt $path_in ]
+         then
             continue
          fi
       fi
       set -x
       g++ -std=c++11 -g -o $path_out $path_in
-      set +x
+      { set +x; } 2>/dev/null
    done
 
    # # Delete output files matching no input file.
-   for path_out in $2/*; do
-      if [ ! -f $path_out ]; then
+   for path_out in $2/*
+   do
+      if [ ! -f $path_out ]
+      then
          continue
       fi
-      if  [[ $path_out != *${suffix_out} ]] ; then
+      if  [ $path_out != *$suffix_out ]
+      then
          continue
       fi
-      name="${path_out##*/}"
-      bare="${name%$suffix_out}"
-      path_in="$1/$bare$suffix_in"
-      if [ ! -f $path_in ]; then
+      name=${path_out##*/}
+      bare=${name%$suffix_out}
+      path_in=$1/${bare}${suffix_in}
+      if [ ! -f $path_in ]
+      then
          set -x
          rm $path_out
-         set +x
+         { set +x; } 2>/dev/null
       fi
    done
 }
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-THIS=`dirname "$0"`
-update "$THIS" "$THIS"
+THIS=$(dirname $0)
+update ${THIS} ${THIS}
