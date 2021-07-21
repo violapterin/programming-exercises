ff15f9fd02795baa21e7695d2e382c88b915a4f2
violapterin <violapterin@gmail.com>
2021-07-22 02:24:08 +0800

    Word break: sketch: use map

--- cpp-medium/word-break.cpp
+++ cpp-medium/word-break.cpp
@@ -8,19 +8,22 @@ times in the segmentation.
    `1 <= text.length <= 300`
    `1 <= dictionary.length <= 1000`
    `1 <= dictionary[i].length <= 20`
-   `s` and `dictionary[i]` consist of only lowercase English letters.
+   `text` and `dictionary[i]` consist of only lowercase English letters.
    All the strings of `dictionary` are unique.
 */
 
 #include <iostream>
 #include <string>
+#include <set>
+#include <map>
 #include <vector>
 #include <algorithm>
 typedef std::vector<std::string> Dictionary;
+typedef std::set<std::string> Group;
+typedef std::map<char, Group> Table;
 struct Order;
 bool shall_break_word(std::string, Dictionary&);
-bool shall_break_word_sorted(std::string, Dictionary&);
-//bool shall_match_beginning(std::string, std::string);
+bool shall_check_table(std::string, Table&);
 
 int main()
 {
@@ -37,39 +40,65 @@ int main()
    // "Yes!"
 }
 
-struct Order {
-   inline bool operator()(
-      const std::string& first,
-      const std::string& second
-   ) const
+/*
+struct Order
+{
+   inline bool operator()(std::string& first, std::string& second)
    {
-      return first.size() >= second.size();
+      return first.size() > second.size();
    }
 };
+*/
 
 bool shall_break_word(std::string text, Dictionary& dictionary)
 {
-   Order order;
-   std::sort(dictionary.begin(), dictionary.end(), order);
-   whether = shall_break_word_sorted(text, dictionary);
-   return whether;
-}
-
-bool shall_break_word_sorted(std::string text, Dictionary& dictionary)
-{
+   std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+   Table table = Table();
+   for (int head = 0; head <= 25; head ++)
+   {
+      char symbol = alphabet[head];
+      Group group = Group();
+      table[symbol] = Group();
+   }
    for (
       auto entry_ = dictionary.begin();
       entry_ != dictionary.end(); entry_++
    )
    {
-      if (text.size() < entry_->size()) { continue; }
-      if (text[0] != (*entry_)[0]) { continue; }
-      int size = entry_->size();
-      std::string slice = text.substr(0, size);
-      if (slice != *entry_) { continue; }
-      if (size == text.size()) { return true; }
-      std::string remain = text.substr(size, std::string::npos);
-      if (shall_break_word_sorted(remain, dictionary)) { return true; }
+      char symbol = (*entry_)[0];
+      table[symbol].insert(*entry_);
+   }
+   /*
+   Order order;
+   for (
+      auto group_ = table.begin();
+      group_ != table.end(); group_++
+   )
+   {
+      std::sort(group_->begin(), group_->end(), order);
+   }
+   */
+   bool whether = shall_check_table(text, table);
+   return whether;
+}
+
+bool shall_check_table(std::string text, Table& table)
+{
+   char symbol = text[0];
+   Group group = table[symbol];
+   for (
+      auto word_ = group.begin();
+      word_ != group.end(); word_++
+   )
+   {
+      int size = word_->size();
+      if (size > text.size()) { continue; }
+      if (text.compare(0, size, *word_) == 0)
+      {
+         if (size == text.size()) { return true; }
+         std::string remain = text.substr(size, std::string::npos);
+         if (shall_check_table(remain, table)) { return true; }
+      }
    }
    return false;
 }
