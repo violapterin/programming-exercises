6d043e4a999dfa91a81891868b80808540e17cba
violapterin <violapterin@gmail.com>
2021-07-25 04:37:54 +0800

    Interleaving strings: Accepted!

--- cpp-medium/interleaving-string.cpp
+++ cpp-medium/interleaving-string.cpp
@@ -17,14 +17,25 @@ where they are divided into nonempty substrings such that:
 
 #include <iostream>
 #include <string>
+#include <tuple>
+#include <map>
 
+typedef std::tuple<int, int, int> Index;
+typedef std::tuple<std::string, std::string, std::string> Text;
+typedef std::map<Index, bool> Record;
 bool be_interleaved(std::string, std::string, std::string);
+void fill_record(Text, Record&, Index);
 
 int main()
 {
+   std::string this_string = "ab";
+   std::string that_string = "bc";
+   std::string goal = "babc";
+   /*
    std::string this_string = "aabcc";
    std::string that_string = "dbbca";
    std::string goal = "aadbbcbcac";
+   */
    if (be_interleaved(this_string, that_string, goal))
    {
       std::cout << "Yes!" << std::endl;
@@ -36,6 +47,115 @@ int main()
    // "Yes!"
 }
 
+bool be_interleaved(
+   std::string this_string,
+   std::string that_string,
+   std::string goal
+)
+{
+   Record record;
+   for (int limit = 0; limit <= goal.size(); limit++)
+   {
+      for (int size = 0; size <= limit; size++)
+      {
+         Text text = std::make_tuple(
+            this_string.substr(0, size),
+            that_string.substr(0, limit - size),
+            goal.substr(0, limit)
+         );
+         Index present = std::make_tuple(
+            size,
+            limit - size,
+            limit
+         );
+         fill_record(text, record, present);
+         //std::cout << "result:" << (record[present] ? "true" : "false") << std::endl;
+      }
+   }
+   Index final = std::make_tuple(
+      this_string.size(),
+      that_string.size(),
+      goal.size()
+   );
+   bool be = record[final];
+   return be;
+}
+
+void fill_record(Text text, Record& record, Index present)
+{
+   std::string text_this = std::get<0>(text);
+   std::string text_that = std::get<1>(text);
+   std::string text_goal = std::get<2>(text);
+   //std::cout << "  goal:" << text_goal << std::endl;
+   //std::cout << "  this:" << text_this << std::endl;
+   //std::cout << "  that:" << text_that << std::endl;
+   int index_this = std::get<0>(present);
+   int index_that = std::get<1>(present);
+   int index_goal = std::get<2>(present);
+   //std::cout << "  index goal:" << index_goal << std::endl;
+   //std::cout << "  index this:" << index_this << std::endl;
+   //std::cout << "  index that:" << index_that << std::endl;
+   if (index_goal != index_this + index_that)
+   {
+      record[present] = false;
+      return;
+   }
+   if (index_this == 0)
+   {
+      if (text_goal == text_that) { record[present] = true; }
+      else { record[present] = false; }
+      return;
+   }
+   if (index_that == 0)
+   {
+      if (text_goal == text_this) { record[present] = true; }
+      else { record[present] = false; }
+      return;
+   }
+   Index last_this = std::make_tuple(
+      index_this - 1,
+      index_that,
+      index_goal - 1
+   );
+   Index last_that = std::make_tuple(
+      index_this,
+      index_that - 1,
+      index_goal - 1
+   );
+   bool whether_this = (text_this.back() == text_goal.back());
+   bool whether_that = (text_that.back() == text_goal.back());
+   if (whether_this) { record[present] = record[last_this]; }
+   if (whether_that) { record[present] = record[last_that]; }
+   if (whether_this && whether_that)
+   {
+      record[present] = record[last_this] || record[last_that];
+   }
+}
+
+   /*
+   if (index_goal == 0)
+   {
+      if (index_this != 0) { record[last] = false; }
+      else if (index_that != 0) { record[last] = false; }
+      else { record[last] = true; }
+      return;
+   }
+   Index next_this = std::make_tuple(
+      index_goal - 1,
+      index_this - 1,
+      index_that
+   );
+   fill_record(record, next_this);
+   Index next_that = std::make_tuple(
+      index_goal - 1,
+      index_this,
+      index_that - 1
+   );
+   fill_record(record, next_this);
+   */
+
+
+/*
 bool be_interleaved(
    std::string this_string,
    std::string that_string,
@@ -86,3 +206,4 @@ bool be_interleaved(
    }
    return false;
 }
+*/
