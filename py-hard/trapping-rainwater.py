7e98d7d65d4e75bc2d74fb1910cec57d41e4ad8c
violapterin <violapterin@gmail.com>
2021-08-14 23:02:21 +0800

    Trapping rainwater: Python: Accepted!

--- py-hard/trapping-rainwater.py
+++ py-hard/trapping-rainwater.py
@@ -8,13 +8,12 @@ it can trap after raining.
    `0 <= size <= 3 * 10^4`
    `0 <= rain[i] <= 10^5`
 '''
-# Accepted ________, 2021
+# Accepted August 14, 2021
 
 import typing as TYPE
 
 def main():
-   #rain = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1] # 6
-   rain = [4,2,0,3,2,5] # 5
+   rain = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1] # 6
    solution = Solution()
    number = solution.count_trap(rain)
    print("There are", number, "drops.")
@@ -23,50 +22,53 @@ def main():
 class Solution:
 
    def count_trap(self, rain: TYPE.List[int]) -> int:
-      print("rain:", rain)
-      if (len(rain) <= 1):
-         return 0
-      left = self.probe_increasing(rain)
-      if left > 0:
-         return self.count_trap(rain[left:])
-      middle = self.probe_decreasing(rain)
-      if middle >= len(rain) - 1:
-         return 0
-      right = middle + self.probe_increasing(rain[middle:])
-      rain_left = rain[:right + 1]
-      rain_right = rain[right:]
-      print("rain_left:", rain_left)
-      print("rain_right:", rain_right)
-      limit = min(rain_left[0], rain_left[-1])
-      water = sum([max(0, limit - value) for value in rain_left])
-      print("water:", water)
-      return water + self.count_trap(rain_right)
+      return self.count_left(rain, 0, len(rain) - 1)
 
-   def probe_decreasing(self, rain: TYPE.List[int]) -> int:
-      if (len(rain) <= 1):
+   def count_left(self,
+      rain: TYPE.List[int],
+      start: int,
+      stop: int
+   ) -> int:
+      if (stop - start <= 1):
          return 0
-      last = rain[0]
-      head = 1
+      left = self.probe_left(rain, start)
+      if (left >= len(rain)):
+         return self.count_right(rain, start, stop)
+      water_left = sum([max(0, rain[start] - value) for value in rain[start:left+1]])
+      water_right = self.count_left(rain, left, stop)
+      return water_left + water_right
+
+   def count_right(self,
+      rain: TYPE.List[int],
+      start: int,
+      stop: int
+   ) -> int:
+      if (stop - start <= 1):
+         return 0
+      right = self.probe_right(rain, stop)
+      if (right < 0):
+         return self.count_left(rain, start, stop)
+      water_right = sum([max(0, rain[stop] - value) for value in rain[right:stop+1]])
+      water_left = self.count_right(rain, start, right)
+      return water_left + water_right
+
+   def probe_right(self, rain: TYPE.List[int], start: int) -> int:
+      limit = rain[start]
+      head = start - 1
+      while (head >= 0):
+         if (rain[head] >= limit):
+            break
+         head -= 1
+      return head
+
+   def probe_left(self, rain: TYPE.List[int], start: int) -> int:
+      limit = rain[start]
+      head = start + 1
       while head < len(rain):
-         if (rain[head] <= last):
-            last = rain[head]
-         else:
+         if (rain[head] >= limit):
             break
          head += 1
-      return head - 1
-
-   def probe_increasing(self, rain: TYPE.List[int]) -> int:
-      if (len(rain) <= 1):
-         return 0
-      last = rain[0]
-      head = 1
-      while head < len(rain):
-         if (rain[head] >= last):
-            last = rain[head]
-         else:
-            break
-         head += 1
-      return head - 1
+      return head
             
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
