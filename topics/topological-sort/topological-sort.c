2b6af5b82f9432eb786eec27ac1ce42e5879b0d9
Raven I. Pilot <violapterin@gmail.com>
2022-12-07 02:27:52 -0500

    topological sort: try compilation

--- topics/topological-sort/topological-sort.c
+++ topics/topological-sort/topological-sort.c
@@ -1,19 +1,27 @@
 #include <stdio.h> // fgets, printf
+#include <stdlib.h> // strtol
 #include <ctype.h> // isalnum
-#define LIMIT_SIZE_FILE 4096
+#include <stdbool.h> // bool
+#define LIMIT_LENGTH 4096
 #define FLAG_USUAL 0
 #define FLAG_COMPLEXITY 1
+
+void sort_topological(int, int, bool**);
+void construct_graph(int, char* , bool**);
+void feed_vertex(int, char*, bool**);
+int find_maximum_numeral(int, char*);
+int find_length(char*);
 
 // argv[0]: input filename
 // argv[1]: flag
 int main(int argc, char** argv)
 {
-   char source[LIMIT_SIZE_FILE];
+   char source[LIMIT_LENGTH];
    FILE* document = fopen(argv[0], "r");
-   fgets(source, LIMIT_SIZE_FILE, document);
+   fgets(source, LIMIT_LENGTH, document);
    fclose(document);
 
-   int length = find_size(source);
+   int length = find_length(source);
    int number_vertex = find_maximum_numeral(length, source);
    bool graph[number_vertex][number_vertex];
    for (int row = 0; row <= number_vertex - 1; row++)
@@ -33,11 +41,22 @@ void sort_topological(int flag, int number_vertex, bool** graph)
       active[row] = false;
    for (int row = 0; row <= number_vertex - 1; row++)
       for (int column = 0; column <= number_vertex - 1; column++) {
-         active[row] = true;
-         active[column] = true;
+         if (graph[row][column]) {
+            active[row] = true;
+            active[column] = true;
+         }
       }
 
    while (true) {
+      bool trivial = true;
+      for (int column = 0; column <= number_vertex - 1; column++)
+         if (active[column]) {
+            trivial = false;
+            break;
+         }
+      if (trivial)
+         break;
+
       int target = 0;
       bool skipped = true;
       for (int column = 0; column <= number_vertex - 1; column++)
@@ -53,6 +72,7 @@ void sort_topological(int flag, int number_vertex, bool** graph)
          if (!with_arrival)
             target = column;
       if (skipped)
+         printf("cycle!");
          break;
 
       printf("vertex:", ' ', target, '\n');
@@ -62,49 +82,63 @@ void sort_topological(int flag, int number_vertex, bool** graph)
    }
 }
 
-void feed_vertex(char* left, int length, bool** graph)
-{
-   int count = 0;
-   char* left = source;
-   char* right = source;
-   while ((*right != '\n') && (*right != ' ')) {
-      right += 1;
-      count += 1;
-   }
-   int numeral_main = strtol(left, NULL, right - left);
-   while (count != length) {
-      if (*right == '\n') {
-         int numeral_another = strtol(left, NULL, right - left);
-         graph[numeral_main][numeral_another] = true;
-         left = right + 1;
-         while (!isalnum(*left))
-            left += 1;
-         right = left;
-         continue;
-      }
-      right += 1;
-      count += 1;
-   }
-   return graph;
-}
-
 void construct_graph(int length, char* source, bool** graph)
 {
    int count = 0;
    char* left = source;
    char* right = source;
    while (count != length) {
-      if (*right == '\n') {
-         feed_vertex(left, right - left, graph);
-         left = right + 1;
-         while (!isalnum(*left))
-            left += 1;
-         right = left;
+      if (isalnum(*right)) {
+         right += 1;
+         count += 1;
          continue;
       }
+      feed_vertex(right - left, left, graph);
+      left = right + 1;
+      while (!isalnum(*left)) {
+         left += 1;
+         count += 1;
+      }
+      right = left;
+   }
+}
+
+void feed_vertex(int length, char* left, bool** graph)
+{
+   int count = 0;
+   char* left = source;
+   char* right = source;
+   while (!isalnum(*left)) {
+      left += 1;
+      count += 1;
+   }
+   right = left;
+   while (isalnum(*right)) {
       right += 1;
       count += 1;
    }
+   int numeral_main = strtol(left, NULL, right - left);
+   while (!isalnum(*left)) {
+      left += 1;
+      count += 1;
+   }
+   right = left;
+   while (count != length) {
+      if (isalnum(*right)) {
+         right += 1;
+         count += 1;
+         continue;
+      }
+      int numeral_another = strtol(left, NULL, right - left);
+      graph[numeral_main][numeral_another] = true;
+      left = right + 1;
+      while (!isalnum(*left)) {
+         left += 1;
+         count += 1;
+      }
+      right = left;
+   }
+   return graph;
 }
 
 int find_maximum_numeral(int length, char* source)
@@ -114,27 +148,29 @@ int find_maximum_numeral(int length, char* source)
    char* left = source;
    char* right = source;
    while (count != length) {
-      if (!isalnum(*right)) {
-         int hold = strtol(left, NULL, right - left);
-         if (hold > maximum)
-            maximum = hold;
-         left = right + 1;
-         while (!isalnum(*left))
-            left += 1;
-         right = left;
+      if (isalnum(*right)) {
+         right += 1;
+         count += 1;
          continue;
       }
-      right += 1;
-      count += 1;
+      int hold = strtol(left, NULL, right - left);
+      if (hold > maximum)
+         maximum = hold;
+      left = right + 1;
+      while (!isalnum(*left)) {
+         left += 1;
+         count += 1;
+      }
+      right = left;
    }
    return maximum;
 }
 
-int find_size(char* source)
+int find_length(char* source)
 {
    int count = 0;
    char* left = source;
-   while ((*right != NULL) && (right - left < LIMIT_SIZE_FILE)) {
+   while ((*right != NULL) && (right - left < LIMIT_LENGTH)) {
       right += 1;
       count += 1;
    }
