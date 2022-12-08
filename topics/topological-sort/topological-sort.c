b9e8b2a4384b5fe47ebb1ead709d09ea97b7f86d
Raven I. Pilot <violapterin@gmail.com>
2022-12-08 00:27:37 -0500

    without seg fault!

--- topics/topological-sort/topological-sort.c
+++ topics/topological-sort/topological-sort.c
@@ -3,30 +3,19 @@
 #include <ctype.h> // isalnum
 #include <stdbool.h> // bool
 #include <string.h> // strlen
-#define LIMIT_LENGTH_LINE 32
-#define FLAG_USUAL 0
-#define FLAG_COMPLEXITY 1
+#define LIMIT_LENGTH_LINE 512
+#define FLAG_USUAL_RESULT_TERMINAL 0
+#define FLAG_USUAL_RESULT_FILE 1
+#define FLAG_COMPLEXITY_RESULT_TERMINAL 2
+#define FLAG_COMPLEXITY_RESULT_FILE 3
 
 void sort_topological(int, int, bool**);
 void construct_vertex(char* , bool**);
 int find_maximum_numeral(char*);
+int count_true(int, bool*);
+void print_graph(int, bool**); // XXX
 
-/*
-// //    stackoverflow.com/questions/11793689/
-// //    read-the-entire-contents-of-a-file-to-c-char-including-new-lines
-FILE *stream;
-char *content;
-stream = fopen(argv[1], "r");
-fseek(stream, 0, SEEK_END);
-int size_file = ftell(stream);
-fseek(stream, 0L, SEEK_SET);
-content = malloc(size_file + 1);
-size_t length = fread(content, 1, size_file, stream);
-content[length] = 0;
-// printf("content:\n%s", content);
-fclose(stream);
-*/
-
+// argv[0]: program name
 // argv[1]: input filename
 // argv[2]: flag
 int main(int argc, char** argv)
@@ -42,7 +31,6 @@ int main(int argc, char** argv)
    fclose(stream_file);
 
    bool* graph[maximum];
-   printf("number vertex: %d\n", maximum);
    for (int row = 0; row <= maximum - 1; row++)
       graph[row] = (bool*) malloc(maximum * sizeof(bool));
    for (int row = 0; row <= maximum - 1; row++)
@@ -55,6 +43,7 @@ int main(int argc, char** argv)
       construct_vertex(content, graph);
    fclose(stream_file);
 
+   print_graph(maximum, graph); // XXX
    int flag = strtol(argv[2], NULL, 1);
    sort_topological(flag, maximum, graph);
 }
@@ -73,44 +62,40 @@ void sort_topological(int flag, int maximum, bool** graph)
       }
 
    while (true) {
-      bool trivial = true;
-      for (int column = 0; column <= maximum - 1; column++)
-         if (active[column]) {
-            trivial = false;
-            break;
-         }
-      if (trivial)
-         break;
+      int active_past = count_true(maximum, active);
+      printf("active past:%d\n", active_past);
 
       int target = 0;
-      bool skipped = true;
       for (int column = 0; column <= maximum - 1; column++) {
          if (!active[column])
             continue;
-         skipped = false;
          bool with_arrival = false;
          for (int row = 0; row <= maximum - 1; row++)
             if (graph[row][column]) {
                with_arrival = true;
                break;
             }
-         if (!with_arrival)
+         if (!with_arrival) {
             target = column;
+         }
       }
-      if (skipped) {
-         printf("cycle!");
-         break;
-      }
-      printf("vertex:%d\n", target + 1); // // 1 based
+      printf("removing vertex:%d\n", target + 1); // // 1 based
       for (int row = 0; row <= maximum - 1; row++)
          graph[row][target] = false;
       active[target] = false;
+
+      int active_current = count_true(maximum, active);
+      printf("active current:%d\n", active_current);
+      if (active_current == active_past) {
+         printf("cycle!\n");
+         break;
+      }
    }
 }
 
 void construct_vertex(char* source, bool** graph)
 {
-   printf("source: %s", source);
+   // printf("source: %s", source); // XXX
    int count = 0;
    int length = strlen(source);
    char* left = source;
@@ -140,7 +125,7 @@ void construct_vertex(char* source, bool** graph)
          continue;
       }
       int numeral_another = strtol(left, NULL, 10);
-      printf("constructing edge %d, %d:\n", numeral_main, numeral_another);
+      // printf("constructing edge %d, %d:\n", numeral_main, numeral_another); // XXX
       graph[numeral_main - 1][numeral_another - 1] = true; // // 0 based
       left = right + 1;
       right = left;
@@ -174,4 +159,27 @@ int find_maximum_numeral(char* source)
       count += 1;
    }
    return maximum;
+}
+
+int count_true(int maximum, bool* array)
+{
+   int active = 0;
+   for (int column = 0; column <= maximum - 1; column++)
+      if (array[column])
+         active += 1;
+   return active;
+}
+
+void print_graph(int maximum, bool** graph)
+{
+   for (int row = 0; row <= maximum - 1; row++) {
+      for (int column = 0; column <= maximum - 1; column++) {
+         if (graph[row][column])
+            printf("1");
+         else
+            printf("0");
+         printf(" ");
+      }
+      printf("\n");
+   }
 }
