51b0c2399a4e4abc24c07b5f22b36b86bfde6411
Raven I. Pilot <violapterin@gmail.com>
2023-04-02 12:19:17 -0400

    use numpy.polynomial consistently

--- topics/information-criteria/deprecated.py
+++ topics/information-criteria/deprecated.py
@@ -4,31 +4,55 @@ import numpy as NP
 import matplotlib.pyplot as PLOT
 import numpy.polynomial.polynomial as PLN
 import numpy.random as RANDOM
+'''
+import warnings as WN
+with WN.catch_warnings():
+   WN.filterwarnings('error')
+   try:
+      estimator = NP.polyfit(sample, observation, degree)
+   except NP.RankWarning:
+      print("not enough data")
+'''
+
+def test():
+   poly = NP.array([0,0,1])
+   x = 2
+   print("poly", poly)
+   print("point", x)
+   print("value", PLN.polyval(x, poly))
 
 '''
 def test():
-   left = -8
-   right = 8
-   for _ in range(20000):
-      poly = RANDOM.default_rng().uniform(left, right, 12)
+   left = -4
+   right = 4
+
+   for _ in range(300):
+      poly = NP.array(RANDOM.default_rng().uniform(left, right, 4))
       square = PLN.polypow(poly, 2)
-      #print("square", square)
       antiderivative = PLN.polyint(square)
-      #print("antiderivative", antiderivative)
-      result = 0
-      result += PLN.polyval(right, antiderivative)
-      result -= PLN.polyval(left, antiderivative)
-      assert(result >= 0)
-      result = NP.log(result)
+
+   poly = NP.array([-2.0, 2.0, 3.3, 0.6])
+   square = PLN.polypow(poly, 2)
+   print("square", square)
+   antiderivative = PLN.polyint(square)
+   print("antiderivative", antiderivative)
+   print("right", NP.polyval(antiderivative, right))
+   print("left", NP.polyval(antiderivative, left))
+
+   try:
+      assert (NP.polyval(antiderivative, right) - NP.polyval(antiderivative, left) > 0)
+   except AssertionError:
+      print("poly", poly)
+      raise
 '''
 
 def main():
-   number_experiment = 12
+   number_experiment = 24
    left = -4
    right = 4
-   degree_correct_low = 12
+   degree_correct_low = 9
    degree_correct_high = 18
-   choice_degree_model_low = [9, 6]
+   choice_degree_model_low = [6, 3]
    choice_degree_model_high = [32, 24]
    choice_number_sample = [90, 120, 150]
 
@@ -110,10 +134,10 @@ def learn(**setup):
    record_bayes = []
 
    for degree in range_degree_model:
-      evaluation = PLN.polyval(sample, correct)
+      evaluation = NP.polyval(correct, sample)
       noise = RANDOM.default_rng().standard_normal(number_sample)
       observation = evaluation + noise
-      estimator = PLN.polyfit(sample, observation, degree)
+      estimator = NP.polyfit(sample, observation, degree)
       loss = find_loss(left, right, correct, estimator)
       likelihood = find_likelihood(correct, estimator)
       akaike = - 2 * likelihood + 2 * degree
@@ -124,26 +148,27 @@ def learn(**setup):
 
    loss_akaike = record_loss[NP.argmin(record_akaike)]
    loss_bayes = record_loss[NP.argmin(record_bayes)]
+   print("record_loss", record_loss)
    return NP.array([loss_akaike, loss_bayes])
 
 def find_likelihood(polynomial_one, polynomial_two):
-   difference = PLN.polysub(polynomial_one, polynomial_two)
+   difference = NP.polysub(polynomial_one, polynomial_two)
    result = 0
    result += - (1/2) * NP.log(2 * NP.pi) * difference.size
    result += NP.sum(- (difference * difference) / 2)
    return result
 
 def find_loss(left, right, polynomial_one, polynomial_two):
-   difference = PLN.polysub(polynomial_one, polynomial_two)
+   difference = NP.polysub(polynomial_one, polynomial_two)
    square = PLN.polypow(difference, 2)
    print("square", square)
    antiderivative = PLN.polyint(square)
    print("antiderivative", antiderivative)
    result = 0
-   result += PLN.polyval(right, antiderivative)
-   result -= PLN.polyval(left, antiderivative)
-   assert(result >= 0)
-   result = NP.log(result)
+   result += NP.polyval(antiderivative, right)
+   result -= NP.polyval(antiderivative, left)
+   #result = NP.log(result)
+   print("result", result)
    return result
 
 # # # # # # # # # # # # # # # # # # # # # # # #
