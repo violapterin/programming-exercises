d41207f571a312d6538242a91faeb7db1b4bc00f
Raven I. Pilot <violapterin@gmail.com>
2023-04-02 02:52:17 -0400

    information criterion: sketch main

--- topics/information-criteria/main.py
+++ topics/information-criteria/main.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as PL
 import numpy as NP
+import matplotlib.pyplot as PLOT
+import numpy.polynomial as PLN
 
-x = NP.linspace(0, 10, 100)
-y_1 = 4 + 2 * NP.sin(2 * x)
-y_2 = 2 + 3 * NP.sin(4 * x)
+def main():
+   left = -8
+   right = 8
+   degree_maximum = 20
+   degree_true = 5
+   number_sample = 20
+   root = RANDOM.default_rng().uniform(degree_true, left, right)
+   sample = RANDOM.default_rng().uniform(number_sample, left, right)
+   true = PLN.polyfromroots(root)
 
-PL.plot(x, y_1)
-PL.plot(x, y_2)
-PL.xlabel("X-axis data")
-PL.ylabel("Y-axis data")
-PL.title('Information Criteria')
-PL.savefig('foo.png', dpi=300)
+   record_loss = zeros(degree_maximum)
+   record_akaike = zeros(degree_maximum)
+   record_bayes = zeros(degree_maximum)
+   for degree_fitted in range(1, degree_maximum + 1):
+      evaluation = NP.polyval(true, sample)
+      noise = RANDOM.default_rng().standard_normal(number_sample)
+      observation = evaluation + noise
+      estimator = NP.polyfit(sample, observation, degree_fitted)
+      loss = find_loss(true, estimator, left, right)
+      likelihood = find_likelihood(true, estimator)
 
+'''
+   x = NP.linspace(0, 10, 100)
+   y_1 = 4 + 2 * NP.sin(2 * x)
+   y_2 = 2 + 3 * NP.sin(4 * x)
+
+   PLOT.plot(x, y_1)
+   PLOT.plot(x, y_2)
+   PLOT.xlabel("X-axis data")
+   PLOT.ylabel("Y-axis data")
+   PLOT.title('Information Criteria')
+   PLOT.savefig('foo.png', dpi=300)
+'''
+
+def find_loss(polynomial_one, polynomial_two, left, right):
+   difference = NP.polysub(polynomial_one, polynomial_two)
+   square = difference.polypow()
+   antiderivative = square.integ()
+   result = (
+      NP.polyval(antiderivative, right)
+      - NP.polyval(antiderivative, left)
+   )
+   return result
