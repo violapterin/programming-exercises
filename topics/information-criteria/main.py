bcf4801ef03d98ed69e2692af9bfa05c20268134
Raven I. Pilot <violapterin@gmail.com>
2023-04-02 12:51:33 -0400

    filter overflowed experiments

--- topics/information-criteria/main.py
+++ topics/information-criteria/main.py
@@ -4,6 +4,8 @@ import numpy as NP
 import matplotlib.pyplot as PLOT
 import numpy.polynomial.polynomial as PLN
 import numpy.random as RANDOM
+from time import time
+
 
 '''
 def test():
@@ -23,15 +25,16 @@ def test():
 '''
 
 def main():
-   number_experiment = 12
+   number_experiment = 128
    left = -4
    right = 4
    degree_correct_low = 12
    degree_correct_high = 18
-   choice_degree_model_low = [9, 6]
+   choice_degree_model_low = [6, 9]
    choice_degree_model_high = [32, 24]
-   choice_number_sample = [90, 120, 150]
+   choice_number_sample = [64, 96]
 
+   time_start = time()
    for number_sample in choice_number_sample:
       print("number of sample", number_sample)
       for degree_model_low in choice_degree_model_low:
@@ -48,6 +51,10 @@ def main():
                degree_correct_low = degree_correct_low,
                degree_correct_high = degree_correct_high,
             )
+   time_end = time()
+   time_total = (time_end - time_start) / 60
+   print("time taken:", f"{time_total:.3f}", "minutes")
+
 
 def simulate(**setup):
    left = setup["left"]
@@ -58,6 +65,7 @@ def simulate(**setup):
    degree_model_high = setup["degree_model_high"]
    degree_correct_low = setup["degree_correct_low"]
    degree_correct_high = setup["degree_correct_high"]
+   threshold = 1e3
 
    range_degree_correct = range(degree_correct_low, degree_correct_high + 1)
    akaike = []
@@ -67,7 +75,7 @@ def simulate(**setup):
       print("      ", "calculating for true degree", degree_correct)
       total = NP.array([0.0, 0.0])
       for _ in range(number_experiment):
-         total += learn(
+         hold = learn(
             left = left,
             right = right,
             number_sample = number_sample,
@@ -75,12 +83,15 @@ def simulate(**setup):
             degree_model_high = degree_model_high,
             degree_correct = degree_correct,
          )
+         if (NP.abs(total[0]) > threshold): continue
+         if (NP.abs(total[1]) > threshold): continue
+         total += hold
       total /= number_experiment
       akaike.append(total[0])
       bayes.append(total[1])
 
-   PLOT.plot(range_degree_correct, akaike)
-   PLOT.plot(range_degree_correct, bayes)
+   PLOT.plot(range_degree_correct, akaike, label = "akaike")
+   PLOT.plot(range_degree_correct, bayes, label = "bayes")
    PLOT.xlabel("degree of true polynomial")
    PLOT.ylabel("squared loss")
    PLOT.title("Information Criteria")
@@ -92,6 +103,7 @@ def simulate(**setup):
       + '.' + "png"
    )
    PLOT.savefig(title, dpi=300)
+   PLOT.clf()
 
 def learn(**setup):
    left = setup["left"]
@@ -100,6 +112,7 @@ def learn(**setup):
    degree_model_low = setup["degree_model_low"]
    degree_model_high = setup["degree_model_high"]
    degree_correct = setup["degree_correct"]
+   threshold = 1e3
 
    root = RANDOM.default_rng().uniform(left, right, degree_correct)
    sample = RANDOM.default_rng().uniform(left, right, number_sample)
@@ -118,6 +131,13 @@ def learn(**setup):
       likelihood = find_likelihood(correct, estimator)
       akaike = - 2 * likelihood + 2 * degree
       bayes = - 2 * likelihood + NP.log(number_sample) * degree
+
+      if (loss > threshold): loss = threshold
+      if (loss < - threshold): loss = - threshold
+      if (akaike > threshold): akaike = threshold
+      if (akaike < - threshold): akaike = - threshold
+      if (bayes > threshold): bayes = threshold
+      if (bayes < - threshold): bayes = - threshold
       record_loss.append(loss)
       record_akaike.append(akaike)
       record_bayes.append(bayes)
@@ -136,18 +156,14 @@ def find_likelihood(polynomial_one, polynomial_two):
 def find_loss(left, right, polynomial_one, polynomial_two):
    difference = PLN.polysub(polynomial_one, polynomial_two)
    square = PLN.polypow(difference, 2)
-   print("square", square)
    antiderivative = PLN.polyint(square)
-   print("antiderivative", antiderivative)
    result = 0
    result += PLN.polyval(right, antiderivative)
    result -= PLN.polyval(left, antiderivative)
-   assert(result >= 0)
-   result = NP.log(result)
    return result
 
 # # # # # # # # # # # # # # # # # # # # # # # #
 
-#main()
+main()
 
-test()
+#test()
