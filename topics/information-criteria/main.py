91186c91ad2898577031a952f4dddf96b5b5dd23
Raven I. Pilot <violapterin@gmail.com>
2023-04-02 08:00:19 -0400

    information criterion: sketch simulation

--- topics/information-criteria/main.py
+++ topics/information-criteria/main.py
@@ -3,47 +3,111 @@
 import numpy as NP
 import matplotlib.pyplot as PLOT
 import numpy.polynomial as PLN
+import numpy.random as RANDOM
 
 def main():
-   left = -8
-   right = 8
-   degree_maximum = 20
-   degree_true = 5
-   number_sample = 20
-   root = RANDOM.default_rng().uniform(degree_true, left, right)
-   sample = RANDOM.default_rng().uniform(number_sample, left, right)
-   true = PLN.polyfromroots(root)
+   NUMBER_EXPERIMENT = 48
+   LEFT = -8
+   RIGHT = 8
+   NUMBER_SAMPLE = 12
+   DEGREE_LOW = 16
+   DEGREE_HIGH = 24
+   DEGREE_MAXIMUM = 32
+   simulate(
+      NUMBER_EXPERIMENT,
+      LEFT,
+      RIGHT,
+      NUMBER_SAMPLE,
+      DEGREE_LOW,
+      DEGREE_HIGH,
+      DEGREE_MAXIMUM,
+   )
 
+def simulate():
+   NUMBER_EXPERIMENT = 48
+   LEFT = -8
+   RIGHT = 8
+   NUMBER_SAMPLE = 12
+   DEGREE_LOW = 16
+   DEGREE_HIGH = 24
+   DEGREE_MAXIMUM = 32
+   range_degree = range(DEGREE_LOW, DEGREE_HIGH + 1)
+   akaike = []
+   bayes = []
+
+   for degree in range_degree:
+      total = [0, 0]
+      for _ in range(NUMBER_EXPERIMENT):
+         total += learn(
+            left = LEFT,
+            right = RIGHT,
+            number_sample = NUMBER_SAMPLE,
+            degree_maximum = DEGREE_MAXIMUM,
+            degree_correct = degree,
+         )
+      total /= NUMBER_EXPERIMENT
+      akaike.append(total[0])
+      bayes.append(total[1])
+
+   PLOT.plot(NP.array(range_correct), NP.array(akaike))
+   PLOT.plot(NP.array(range_correct), NP.array(bayes))
+   PLOT.xlabel("degree of true polynomial")
+   PLOT.ylabel("squared loss")
+   PLOT.title("Information Criteria")
+   title = (
+      "plot"
+      + '-' + "degree" + '-' + str(DEGREE_LOW) + '-' + str(DEGREE_HIGH)
+      + '-' + "maximum" + '-' + str(DEGREE_MAXIMUM)
+      + '-' + "sample" + '-' + str(NUMBER_SAMPLE)
+      + '.' + "png"
+   )
+   PLOT.savefig(title, dpi=300)
+
+
+def learn(**setup):
+   left = setup[left]
+   right = setup[right]
+   number_sample = setup[number_sample]
+   degree_maximum = setup[degree_maximum]
+   degree_correct = setup[degree_correct]
+
+   root = RANDOM.default_rng().uniform(degree_correct, left, right)
+   sample = RANDOM.default_rng().uniform(number_sample, left, right)
+   correct = PLN.polyfromroots(root)
    record_loss = zeros(degree_maximum)
    record_akaike = zeros(degree_maximum)
    record_bayes = zeros(degree_maximum)
-   for degree_fitted in range(1, degree_maximum + 1):
-      evaluation = NP.polyval(true, sample)
+
+   for degree in range(1, degree_maximum + 1):
+      evaluation = NP.polyval(correct, sample)
       noise = RANDOM.default_rng().standard_normal(number_sample)
       observation = evaluation + noise
-      estimator = NP.polyfit(sample, observation, degree_fitted)
-      loss = find_loss(true, estimator, left, right)
-      likelihood = find_likelihood(true, estimator)
+      estimator = NP.polyfit(sample, observation, degree)
+      loss = find_loss(left, right, correct, estimator)
+      likelihood = find_likelihood(correct, estimator)
+      akaike = - 2 * likelihood + 2 * degree
+      bayes = - 2 * likelihood + NP.log(number_sample) * degree
+      record_loss[degree] = loss
+      record_akaike[degree] = akaike
+      record_bayes[degree] = bayes
 
-'''
-   x = NP.linspace(0, 10, 100)
-   y_1 = 4 + 2 * NP.sin(2 * x)
-   y_2 = 2 + 3 * NP.sin(4 * x)
+   loss_akaike = loss[numpy.argmin(record_akaike)]
+   loss_bayes = loss[numpy.argmin(record_bayes)]
+   return [loss_akaike, loss_bayes]
 
-   PLOT.plot(x, y_1)
-   PLOT.plot(x, y_2)
-   PLOT.xlabel("X-axis data")
-   PLOT.ylabel("Y-axis data")
-   PLOT.title('Information Criteria')
-   PLOT.savefig('foo.png', dpi=300)
-'''
+def find_likelihood():
+   difference = NP.polysub(polynomial_one, polynomial_two)
+   result = 0
+   result += - (1/2) * NP.log(2 * NP.pi) * difference.size
+   result += np.sum(- (difference * difference) / 2)
+   return result
 
-def find_loss(polynomial_one, polynomial_two, left, right):
+def find_loss(left, right, polynomial_one, polynomial_two):
    difference = NP.polysub(polynomial_one, polynomial_two)
    square = difference.polypow()
    antiderivative = square.integ()
-   result = (
-      NP.polyval(antiderivative, right)
-      - NP.polyval(antiderivative, left)
-   )
+   result = 0
+   result = NP.polyval(antiderivative, right)
+   result =  NP.polyval(antiderivative, left)
    return result
+
