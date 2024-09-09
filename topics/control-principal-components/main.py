bcf250168a27ac79378cb0ee63ab7e49e578d739
Raven I. Pilot <violapterin@gmail.com>
2024-09-09 17:58:03 -0400

    (deprecate project on control)

--- topics/control-principal-components/main.py
+++ topics/control-principal-components/main.py
@@ -8,36 +8,49 @@ import datetime
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 def main(situation):
-   number_sample_original = 16
+   number_sample_train = 16
    number_sample_experiment = 16
    ratio_singular_kept = [0.8 + 0.05 * x for in list(range(4))]
    many_sample_yy = []
    many_sample_uu = []
    many_sample_vv = []
 
-   for _ in range(number_sample_original):
+   for _ in range(number_sample_train):
       hold = run_linear_quadratic_regulator(
-         many_sample_yy = None,
-         many_sample_uu = None,
-         whether_drop_singular_values = False,
-         whether_print_yy = True,
-         whether_print_uu = True,
-         whether_print_vv = False,
+         horizon = 64
+         size_xx = 16
+         size_uu = 12
+         size_yy = 8
+         factor_cost = 1
+         number_experiment = 32
       )
       many_sample_yy = hold[0]
       many_sample_uu = hold[1]
 
+   many_reduction = [] # XXX
+   for index_tt in range(horizon):
+      variance_ll_uu = NP.cov(many_sample_uu[index_tt], many_sample_uu[index_tt])
+      variance_ll_yy_uu = NP.cov(many_sample_yy[index_tt], many_sample_uu[index_tt])
+      variance_ll_yy = NP.cov(many_sample_yy[index_tt], many_sample_yy[index_tt])
+      unitary_gg, singular_ss, unitary_ff_tt = NP.linalg.svd(
+         NP.linalg.inv(variance_ll_yy)
+         @ variance_ll_yy_uu
+         @ NP.linalg.inv(variance_ll_uu)
+      )
+      unitary_gg_tt = NP.transpose(unitary_gg)
+      unitary_ff = NP.transpose(unitary_ff_tt)
+      many_reduction.append(projection_ff)
+
    for _ in range(number_sample_experiment):
-      for _ in range(number_sample_experiment):
-         hold = run_linear_quadratic_regulator(
-            many_sample_yy = many_sample_yy,
-            many_sample_uu = many_sample_uu,
-            whether_drop_singular_values = True,
-            whether_print_yy = False,
-            whether_print_uu = False,
-            whether_print_vv = True,
-         )
-         many_sample_vv = hold[0]
+      hold = run_linear_quadratic_regulator(
+         many_projection_cc = many_projection_cc,
+         horizon = 64
+         size_xx = 16
+         size_uu = 12
+         size_yy = 8
+         factor_cost = 1
+      )
+      many_sample_vv = hold[0]
 
    '''
    count_line = 0
@@ -69,29 +82,27 @@ def main(situation):
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 def run_linear_quadratic_regulator(
-   many_sample_yy,
-   many_sample_uu,
-   whether_drop_singular_values,
-   whether_print_yy,
-   whether_print_uu,
-   whether_print_vv,
+   many_reduction,
+   horizon,
+   size_xx,
+   size_uu,
+   size_yy,
+   factor_cost,
 ):
-   horizon = 64
-   size_xx = 16
-   size_uu = 12
-   size_yy = 8
-   factor_cost = 1
-   number_experiment = 32
 
    identity_xx = NP.eyes(size = (size_xx, size_xx))
    identity_uu = NP.eyes(size = (size_uu, size_uu))
    identity_yy = NP.eyes(size = (size_yy, size_yy))
    dynamics_aa = NP.random.normal(0, 1, size = (size_xx, size_xx))
    dynamics_bb = NP.random.normal(0, 1, size = (size_xx, size_uu))
-   projection_cc = NP.random.normal(0, 1, size = (size_xx, size_yy))
    dynamics_aa_tt = NP.transpose(dynamics_aa)
    dynamics_bb_tt = NP.transpose(dynamics_bb)
-   projection_cc_tt = NP.transpose(projection_cc)
+   for index_tt in range(horizon):
+      projection_cc = (
+         many_reduction[index_tt]
+         @ NP.random.normal(0, 1, size = (size_xx, size_yy))
+      )
+      projection_cc_tt = NP.transpose(projection_cc)
 
    mean_mm_present_last = NP.zeros(size = size_xx)
    variance_ss_present_last = NP.zeros(size = (size_xx, size_xx))
@@ -123,24 +134,8 @@ def run_linear_quadratic_regulator(
    many_control_uu = [[] for _ in range(horizon)]
    many_value_vv = []
 
-   for index_tt in range(horizon):
-      variance_ll_yy = NP.cov(many_sample_uu[index_tt], many_sample_uu[index_tt])
-      variance_ll_yy_uu = NP.cov(many_sample_yy[index_tt], many_sample_uu[index_tt])
-      variance_ll_yy = NP.cov(many_sample_yy[index_tt], many_sample_yy[index_tt])
-      unitary_gg, singular_ss, unitary_ff_tt = NP.linalg.svd(
-         NP.linalg.inv(variance_ll_yy)
-         @ variance_ll_yy_uu
-         @ NP.linalg.inv(variance_ll_uu)
-      )
-      unitary_gg_tt = NP.transpose(unitary_gg)
-      unitary_ff = NP.transpose(unitary_ff_tt)
-      projection_ff = unitary_ff[] # XXX
-      equivalent_cc = projection_cc
-      equivalent_cc_tt = projection_cc_tt
-      if whether_drop_singular_values:
-         equivalent_cc = projection_ff @ projection_cc
-         equivalent_cc_tt = NP.transpose(equivalent_cc)
 
+   for index_tt in range(horizon):
       noise_ww = NP.random.normal(0, 1, size = size_xx)
       noise_vv = NP.random.normal(0, 1, size = size_yy)
       state_xx = (
